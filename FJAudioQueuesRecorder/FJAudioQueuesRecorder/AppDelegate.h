5016313114ceca4aaa9f05f2e71b0b39714a1c5f
jiamingzhou <1058957590@qq.com>
2018-07-29 22:39:39 +0800

    添加工程

---
+++ FJAudioQueuesRecorder/FJAudioQueuesRecorder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FJAudioQueuesRecorder
+//
+//  Created by 贾明洲 on 2018/7/29.
+//  Copyright © 2018年 FisherJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
